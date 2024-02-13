6d0f39bd5357861a2034128e4de3409b0f11e1b2
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-02-13 15:39:59 -0300

    função de pulo adicionada.
    Adicionada a função de pulo com 800 de forcaPulo e uma Gravity Scale de 5.

---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float forcaPulo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Pular();
+        }
+    }
+
+    void Pular()
+    {
+        rb.AddForce(Vector2.up * forcaPulo);
+    }
+}
