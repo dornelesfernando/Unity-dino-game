903679d0b50d8cc40939f0894df12476b29758c5
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-02-13 22:53:57 -0300

    Geração de cactos aleatórios

--- Assets/Scripts/GeradorCacto.cs
+++ Assets/Scripts/GeradorCacto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
@@ -6,7 +7,7 @@ using UnityEngine;
 
 public class GeradorCacto : MonoBehaviour
 {
-    public GameObject cactoPrefab;
+    public GameObject[] cactoPrefabs;
     public float delayInicial;
     public float delayEntreCactos;
     // Start is called before the first frame update
@@ -18,6 +19,9 @@ public class GeradorCacto : MonoBehaviour
     // Update is called once per frame
     private void GerarCacto()
     {
+        var quantidadeCactos = cactoPrefabs.Length;
+        var indiceAleatorio = UnityEngine.Random.Range(0, quantidadeCactos);
+        var cactoPrefab = cactoPrefabs[indiceAleatorio];
         Instantiate(cactoPrefab, transform.position, quaternion.identity);
     }
 }
