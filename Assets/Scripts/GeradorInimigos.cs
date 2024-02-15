3947e51914d0dd838a1c877b1eb684bd83aded0c
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-02-15 02:19:55 -0300

    Colisor do dinossauro corrigido e implementação de inimigos por distancia
    Colisor do dinossauro corrigido e implementação de inimigos por distância "Fixa"

--- Assets/Scripts/GeradorInimigos.cs
+++ Assets/Scripts/GeradorInimigos.cs
@@ -15,36 +15,63 @@ public class GeradorInimigos : MonoBehaviour
     public Jogador jogadorScript;
     public float delayInicial;
     public float delayEntreCactos;
+    public float distanciaMinima = 4;
+    public float distancaiMaxima = 8;
     // Start is called before the first frame update
     private void Start()
     {
-        InvokeRepeating("GerarInimigos", delayInicial, delayEntreCactos);
+        // InvokeRepeating("GerarInimigos", delayInicial, delayEntreCactos);
+        StartCoroutine(GerarInimigos());
     }
 
     // Update is called once per frame
-    private void GerarInimigos()
+    private IEnumerator GerarInimigos()
     {
-        var random = UnityEngine.Random.Range(1, 7);
+        yield return new WaitForSeconds(delayInicial);
 
-        if (jogadorScript.pontos >= pontuacaoDinossaroVoadorMinima && random <= 2)
+        GameObject ultimoInimigoGerado = null;
+
+        var distanciaNecessaria = 0f;
+
+        while (true)
         {
-            var posicaoYAleatoria = UnityEngine.Random.Range(dinossauroVoadorYMinimo, dinossauroVoadorYMaximo);
+            var geracaoObjetoLiberada = ultimoInimigoGerado == null
+             || UnityEngine.Vector3.Distance(transform.position, ultimoInimigoGerado.transform.position) >= distanciaNecessaria;
 
-            var posicao = new UnityEngine.Vector3(
-                transform.position.x,
-                transform.position.y + posicaoYAleatoria,
-                transform.position.z
-            );
+            print(distanciaNecessaria);
 
-            Instantiate(dinossauroVoadorPrefab, posicao, quaternion.identity);
+            if (ultimoInimigoGerado != null) print(UnityEngine.Vector3.Distance(transform.position, ultimoInimigoGerado.transform.position));
+
+            if (geracaoObjetoLiberada)
+            {
+                var random = UnityEngine.Random.Range(1, 7);
+
+                if (jogadorScript.pontos >= pontuacaoDinossaroVoadorMinima && random <= 2)
+                {
+                    var posicaoYAleatoria = UnityEngine.Random.Range(dinossauroVoadorYMinimo, dinossauroVoadorYMaximo);
+
+                    var posicao = new UnityEngine.Vector3(
+                        transform.position.x,
+                        transform.position.y + posicaoYAleatoria,
+                        transform.position.z
+                    );
+
+                    ultimoInimigoGerado = Instantiate(dinossauroVoadorPrefab, posicao, quaternion.identity);
+                }
+                else
+                {
+                    var quantidadeCactos = cactoPrefabs.Length;
+                    var indiceAleatorio = UnityEngine.Random.Range(0, quantidadeCactos);
+                    var cactoPrefab = cactoPrefabs[indiceAleatorio];
+                    ultimoInimigoGerado = Instantiate(cactoPrefab, transform.position, quaternion.identity);
+                }
+
+                distanciaNecessaria = UnityEngine.Random.Range(distanciaMinima, distancaiMaxima);
+            }
+
+            yield return null;
+
+            // yield return new WaitForSeconds(delayEntreCactos);
         }
-        else
-        {
-            var quantidadeCactos = cactoPrefabs.Length;
-            var indiceAleatorio = UnityEngine.Random.Range(0, quantidadeCactos);
-            var cactoPrefab = cactoPrefabs[indiceAleatorio];
-            Instantiate(cactoPrefab, transform.position, quaternion.identity);
-        }
-
     }
 }
